f2fbc6b1a3be24b4b0c28a116713ef3a021bd3b7
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-02 15:44:08 +0900

    Bike Shop App 3.12
    3.12 Update

---
+++ LocalObjectApp/LocalObjectApp/MyData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalObjectApp
+{
+    namespace Local
+    {
+        public class MyData
+        {
+            public int DataInt { get; set; }
+            public string DataStr { get; set; }
+        }
+    }
+}
