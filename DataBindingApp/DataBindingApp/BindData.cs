9c968cb517bf5602182c82cc7246fc9464a175be
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-02 16:55:02 +0900

    Binding!!!
    Binding!!!

---
+++ DataBindingApp/DataBindingApp/BindData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingApp
+{
+    public class BindData
+    {
+        public string DataStr { get; set; }
+        public int DataInt { get; set; }
+
+        public BindData() : this("", 0)
+        {
+
+        }
+        public BindData(string s, int n)
+        {
+            DataStr = s;
+            DataInt = n;
+        }
+    }
+}
