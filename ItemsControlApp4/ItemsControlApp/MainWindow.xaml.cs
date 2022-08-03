5bd0aa89340961263a08b33535446427cbe4e5fd
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-03 17:31:01 +0900

    day2 final
    day2 final

--- ItemsControlApp4/ItemsControlApp/MainWindow.xaml.cs
+++ ItemsControlApp4/ItemsControlApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,34 +23,33 @@ namespace ItemsControlApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        public StrList List { get; set; }
+        public PersonList List { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            List = new StrList();
-            List.Add("Hello!");
-            List.Add("Hi~!");
-            
+            List = new PersonList();
             this.DataContext = this;
-            //this.DataContext = new StrList();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {//왼쪽
-            List.Add(tb1.Text);
+        {
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
-        {//오른쪽
-            //FindResource() 방법 :검색에서 리소스를 찾음
-            //Resources 속성 사용하는 방법 : 객체에서만 리소스를 찾음
-            //var lt = this.FindResource("lt") as StrList;
-            var lt = lb.FindResource("lt") as StrList;
-            //var lt = this.Resources["lt"] as StrList;
-            lt.Add(tb2.Text);
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            MessageBox.Show(btn.DataContext.ToString());
         }
     }
-    //public class StrList : List<string>
+
+
     public class StrList : ObservableCollection<string>
     {
         public string ListName => "연습용 리스트";
@@ -57,6 +58,98 @@ namespace ItemsControlApp
             Add("ABC");
             Add("DEF");
             Add("GIH");
+        }
+    }
+
+    public class PersonList : ObservableCollection<Person>
+    {
+        public string ListName => "연습용 리스트";
+        public PersonList()
+        {
+            Add(new Person() { Name = "고영희", Age = 23, Clr = Colors.Beige });
+            Add(new Person() { Name = "강아쥐", Age = 41, Clr = Colors.Violet });
+            Add(new Person() { Name = "햄스톨", Age = 18, Clr = Colors.SkyBlue });
+
+        }
+    }
+
+
+
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+    public class Person : Notifier
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+                //부가적인 작업
+            }
+        }
+        private Color clr;
+        public Color Clr
+        {
+            get { return clr; }
+            set
+            {
+                clr = value;
+                OnPropertyChanged("Clr");
+
+
+            }
+        }
+        public Person()
+        {
+            Name = "noname";
+            Age = 0;
+            Clr = Colors.White;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name : {Name}, Age : {Age}, Color : {Clr}";
+        }
+    }
+
+    public class ColorConverter : IValueConverter
+    {
+        //소스 -> 타겟 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color Clr = (Color)value;
+            return new SolidColorBrush(Clr);
+        }
+
+
+        //타겟 -> 소스
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
         }
     }
 }
