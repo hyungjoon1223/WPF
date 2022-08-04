f41175cf847ec51d1e2fa0a92812df8ac77f0635
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-04 15:25:09 +0900

    4.10 test
    test suc

--- BikeShop/BikeShop/Menu.xaml.cs
+++ BikeShop/BikeShop/Menu.xaml.cs
@@ -34,5 +34,10 @@ namespace BikeShop
         {
             NavigationService.Navigate(new Uri("/Discussion.xaml", UriKind.Relative));
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ProductsManagement.xaml", UriKind.Relative));
+        }
     }
 }
---
+++ BikeShop/BikeShop/Talk.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace BikeShop
+{
+    public class Talk : ObservableCollection<Message>
+    {
+        public Talk()
+        {
+            this.Add(new Message() { Sender = "Adventure Works", Content = "Hi, what can we do for you?" });
+            this.Add(new Message() { Sender = "Client", Content = "Did you receive the GR268 KZ bike?" });
+            this.Add(new Message() { Sender = "Adventure Works", Content = "Not yet, but we have a similar model available." });
+            this.Add(new Message() { Sender = "Client", Content = "What is it like?" });
+            this.Add(new Message() { Sender = "Adventure Works", Content = "It boasts a carbon frame, hydraulic brakes and suspension, and a gear hub." });
+            this.Add(new Message() { Sender = "Client", Content = "How much does it cost?" });
+            this.Add(new Message() { Sender = "Adventure Works", Content = "Same as the GR268 KZ model you requested. You can get it from our online shop." });
+            this.Add(new Message() { Sender = "Client", Content = "Thanks." });
+            this.Add(new Message() { Sender = "Adventure Works", Content = "Thank you, have a nice ride." });
+        }
+    }
+
+    public class Message
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; }
+    }
+}
