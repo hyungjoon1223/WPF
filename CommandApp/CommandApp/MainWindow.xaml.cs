2e605d819ea7cf66cf1c4abd30e01a74513e9a05
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-08 08:31:36 +0900

    WPF Command
    WPF Command

--- CommandApp/CommandApp/MainWindow.xaml.cs
+++ CommandApp/CommandApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,75 @@ namespace CommandApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Person Person { get; set; }
+        public PersonInfoCommand PerInfoCommand { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            Person = new Person("홍길동", 30);
+            PerInfoCommand = new PersonInfoCommand(Action_PersonInfo);
+
+            this.DataContext = this;
+        }
+        public void Action_PersonInfo(object param)
+        {
+            MessageBox.Show(Person.ToString());
+        }
+        public void Action_CallMethod(object sender, RoutedEventArgs e)
+        {
+            ++Person.Age;
+        }
+    }
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name = null;
+        public string Name { get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this , new PropertyChangedEventArgs("Name"));
+            }
+        }
+        private int age = 0;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Age"));
+            }
+        }
+        public Person(string n = "noname", int a= 0)
+        {
+            Name = n;
+            Age = a;
+        }
+        public override string ToString()
+        {
+            return $"Name:{Name}, Age{Age}";
+        }
+    }
+    public class PersonInfoCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> cmdMethod;
+        public PersonInfoCommand(Action<object> cm)
+        {
+            cmdMethod = cm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //체크박스를 누르면 true 안눌려있으면 false
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            cmdMethod(parameter);
         }
     }
 }
