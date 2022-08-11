da208e585b0a91c17af95172ad1309848d51a683
황형준 <71005596+hyungjoon1223@users.noreply.github.com>
2022-08-12 08:37:04 +0900

    WPF 마무링!
    WPF 마무링!

---
+++ BikeShop (마무리 페이지생성)/BikeShop/ViewModels/Commands/MessageCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BikeShop.ViewModels.Commands
+{
+    internal class MessageCommand : ICommand
+    {
+        private Action _execute;
+        public event EventHandler CanExecuteChanged;
+
+        public MessageCommand(Action execute)
+        {
+            _execute = execute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+    }
+}
---
+++ BikeShop (마무리 페이지생성)/BikeShop/ViewModels/MessageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BikeShop.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BikeShop.ViewModels
+{
+    internal class MessageViewModel
+    {
+        public MessageCommand DisplayMessageCommand { get; set; }
+
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+        public void DisplayMessage()
+        {
+            MessageBox.Show("클릭했쌈");
+
+        }
+    }
+}
